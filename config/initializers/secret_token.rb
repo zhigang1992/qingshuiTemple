52cd4f6700311ab800d38647c255e226066d9d8e
fahchen <06fahchen@gmail.com>
2013-01-30 14:41:15 +0800

    rails 4 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QingshuiTemple::Application.config.secret_key_base = 'd0dd364983b186a601ed8f6d9da2a9422568830e761902a75a9c9f673bf67aa830aa67cd2355fae044512f59e82798fb1c382f03ee543f25df2774bec09603e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QingshuiTemple::Application.config.session_store :encrypted_cookie_store, key: '_qingshuiTemple_session'
